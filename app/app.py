33b83dcec118bbf651ff06ab3141c0d045d8add0
Benedict Solomon <solomon103098@gmail.com>
2021-05-25 11:31:52 +0800

    catch add json to request

--- app/app.py
+++ app/app.py
@@ -15,14 +15,21 @@ def wordcount():
             }
     '''
     usr_input = request.json
-    if checkUrl(usr_input['url']):
-        results = getSiteTextCount(usr_input['url'],usr_input['word'])
-        if results['status'] == 'ok':
-            addCount(usr_input['word'], usr_input['url'],results['count'])
-        return results
+    if usr_input:
+        if checkUrl(usr_input['url']):
+            results = getSiteTextCount(usr_input['url'],usr_input['word'])
+            if results['status'] == 'ok':
+                addCount(usr_input['word'], usr_input['url'],results['count'])
+            return results
+        else:
+            to_return = {
+                'status' : 'fail',
+                'msg' : 'could not parse the page'
+            }
+            return to_return
     else:
         to_return = {
             'status' : 'fail',
-            'msg' : 'could not parse the page'
+            'msg' : 'provide json'
         }
         return to_return
